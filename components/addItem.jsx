295ba3d683470f3d89b30525f09b5bd468509657
SamirSutar <prajwal.pawar@tipic.in>
2024-09-14 14:05:21 +0530

    addTable backround

--- components/addItem.jsx
+++ components/addItem.jsx
@@ -102,7 +102,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    backgroundColor: '#f8f8f8',
+    backgroundColor: 'gray',
+    borderWidth:3,
+    borderRadius:30,
   },
   filterInput: {
     marginBottom: 20,
